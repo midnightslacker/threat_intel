15b80c045a61c6115acfc607895020753d943458
midnightslacker <midnightslacker11@gmail.com>
2014-08-18 14:21:29 -0400

    grabs additional threat sources

---
+++ error_bypass.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# malwareBytes hpHosts
+echo 'Grabbing malwareBytes Hosts.txt'
+curl -o ./hosts.txt http://hosts-file.net/download/hosts.txt
+grep 127\.0\.0\.1 hosts.txt | sed -e "1d" | sed -e 's/127\.0\.0\.1[\t]//g' > $HOME/dev/threat_domains/malwareBytes_hpHosts
+rm -f ./hosts.txt
+
+# tor nodes
+echo 'Grabbing tor nodes list'
+curl -k -o ./tor_nodes https://www.dan.me.uk/torlist/
+python $HOME/bin/filter_ips ./tor_nodes $HOME/dev/threat_sources/tor_nodes
+rm -f ./tor_nodes
+
+# ssl blacklist -- Currently offline
+echo 'Grabbing ssl blacklist' 
+curl -k -o ./ssl_blacklist  https://sslbl.abuse.ch/blacklist/sslipblacklist.rules
+python $HOME/bin/filter_ips ./ssl_blacklist $HOME/dev/threat_sources/ssl_blacklist
+rm -f ./ssl_blacklist
